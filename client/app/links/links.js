2d78bd0ab6ad37fb4ea66e37627678f2c8ca070a
doctown <doctown@yahoo.com>
2016-04-01 15:27:09 -0700

    working on links html

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,24 +1,10 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
-
-  // $scope.link = {};
+  // store all links
+  // Initialize links with all links stored on server from getAll call
   $scope.data = {};
   Links.getAll().then(function(data) {
     $scope.data.links = data;
   });
-  
-  // store all links
-  // Initialize links with all links stored on server from getAll call
-
-  $scope.addOne = function() {
-    Links.addOne($scope.link).then(function(params) {
-      //after link is added to the database
-        // add link to links
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  };
 });
