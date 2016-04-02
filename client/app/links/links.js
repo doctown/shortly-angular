3b859d84a5788a554964e999a02df92bb82566f3
doctown <doctown@yahoo.com>
2016-04-02 12:30:34 -0700

    finished bare requirements

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,16 +3,23 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links, Auth) {
   // store all links
   // Initialize links with all links stored on server from getAll call
-  $scope.data = {};
-  
-  Links.getAll().then(function(data) {
+
+  $scope.init = function() {
     if (!Auth.isAuth()) {
       Auth.signout();
     }
+  };
+
+  $scope.data = {};
+  
+  Links.getAll().then(function(data) {
     $scope.data.links = data;
   });
 
   $scope.signout = function() {
     Auth.signout();
   };
+
+  $scope.init();
+
 });
